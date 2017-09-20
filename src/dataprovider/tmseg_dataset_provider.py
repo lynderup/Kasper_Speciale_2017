1610d419427a36d54d69a97f72a7c5d2664e61d6
Lynderup <lynderupjensen@gmail.com>
2017-09-20 17:04:49 +0200

    Dataset now repeats indefinitely

--- src/dataprovider/tmseg_dataset_provider.py
+++ src/dataprovider/tmseg_dataset_provider.py
@@ -8,7 +8,7 @@ from dataprovider.fasta_to_tfrecord_converter import fasta_to_tfrecord
 
 def _parse_function(example_proto):
     context_features = {
-        "length": tf.FixedLenFeature([1], dtype=tf.int64)
+        "length": tf.FixedLenFeature([], dtype=tf.int64)
     }
     sequence_features = {
         "sequence": tf.FixedLenSequenceFeature([], dtype=tf.int64),
@@ -18,8 +18,8 @@ def _parse_function(example_proto):
     context_parsed, sequence_parsed = tf.parse_single_sequence_example(serialized=example_proto,
                                                                        context_features=context_features,
                                                                        sequence_features=sequence_features)
-
-    return context_parsed["length"], sequence_parsed["sequence"], sequence_parsed["structure"]
+    lengths = tf.cast(context_parsed["length"], tf.int32)
+    return lengths, sequence_parsed["sequence"], sequence_parsed["structure"]
 
 
 class TMSEGDatasetProvider(DatasetProvider):
@@ -50,8 +50,7 @@ class TMSEGDatasetProvider(DatasetProvider):
             os.makedirs(path + tfrecord_path)
             fasta_to_tfrecord(path, fasta_path, tfrecord_path, sets)
 
-    @staticmethod
-    def get_dataset(batch_size):
+    def get_dataset(self, batch_size):
 
         dataset_path = "datasets/tmseg/data/sets/tfrecords/"
         filename_suffix = ".tfrecord"
@@ -61,14 +60,19 @@ class TMSEGDatasetProvider(DatasetProvider):
 
         dataset = tf.contrib.data.TFRecordDataset(paths)
         dataset = dataset.map(_parse_function)
-        dataset = dataset.padded_batch(batch_size, padded_shapes=([1], [None], [None]))
+        dataset = dataset.repeat(None)  # Infinite iterations
+        dataset = dataset.shuffle(buffer_size=1000)
+        dataset = dataset.padded_batch(batch_size, padded_shapes=([], [None], [None]))
 
         return dataset
 
 
 if __name__ == '__main__':
     TMSEGDatasetProvider.download_and_convert_if_not_existing()
-    dataset = TMSEGDatasetProvider.get_dataset(4)
+
+    dataprovider = TMSEGDatasetProvider()
+
+    dataset = dataprovider.get_dataset(4)
 
     iterator = dataset.make_initializable_iterator()
     length, sequence, structure = iterator.get_next()
